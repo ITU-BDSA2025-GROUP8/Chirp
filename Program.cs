9c552dcb8caa10e49837b69aa87797ac729e59e9
Marie <majoh@itu.dk>
2025-08-29 12:50:29 +0200

    Debug and Reader method
    Debugged for exception in the code and gathered the current code in the Reader method

--- Program.cs
+++ Program.cs
@@ -1,34 +1,53 @@
 ﻿//List<string> cheeps = new() { "Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer.", "Cheeping cheeps on Chirp :)" };
+using System.Globalization;
 
-
-if (args[0] != "read")
+if (args[0] == "read")
 {
-    return;
-}
-try
-{
-    StreamReader input= new StreamReader(args[1]);
-    string line = input.ReadLine();
-
-    while (line != null)
+    try
     {
-        line = input.ReadLine();
-        //time and date
-        int unixTime = int.Parse(line[Range.StartAt(line.LastIndexOf(",")+1)]);
-        DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(unixTime);
-        DateTimeOffset localtime =  date.LocalDateTime;
-        string prettyTime = localtime.ToString("MM/dd/yy HH:mm:ss").Replace("-", "/");
-        
-        //message
-        Range messageRange = new Range(line.IndexOf(",") + 2, line.LastIndexOf(",") - 1);
-        String message = line[messageRange];
-        //print
-        Console.WriteLine(line[Range.EndAt(4)] + " @ " + prettyTime + ": " + message);
+        Reader(args[1]);
     }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+    
 }
-catch (FileNotFoundException e)
+
+static void Reader(string args)
 {
-    Console.WriteLine(e.Message);
+    try
+    {
+        StreamReader input = new StreamReader(args);
+        input.ReadLine(); //skips first line
+
+        while (input.EndOfStream == false)
+        {
+            String line = input.ReadLine();
+            
+            if (line != null)
+            {
+                //time and date
+                int index = line.LastIndexOf(',');
+                string time = line.Substring(index+1);
+                int unixTime = int.Parse(time);
+                DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(unixTime);
+                DateTimeOffset localtime = date.LocalDateTime;
+                string prettyTime = localtime.ToString("MM/dd/yy HH:mm:ss").Replace("-", "/");
+
+                //message
+                Range messageRange = new Range(line.IndexOf(",") + 2, line.LastIndexOf(",") - 1);
+                String message = line[messageRange];
+                
+                //print
+                Console.WriteLine(line[Range.EndAt(4)] + " @ " + prettyTime + ": " + message);
+            }
+        }
+    }
+    catch (Exception e)
+    {
+       Console.WriteLine(e);
+    }
 }
 
 /* goal output:
