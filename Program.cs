668dba5a6debe15b81dd39fb18499804f94ac134
Marie <majoh@itu.dk>
2025-08-29 12:08:06 +0200

    Compeleted exercise 3.1-3.3
    Read from CSV file given using dotnet run -- read command followed by the file path in the commandline. Outputs the data in a more readable format as required to complete the exercises

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
-﻿
-foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿//List<string> cheeps = new() { "Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer.", "Cheeping cheeps on Chirp :)" };
 
-/*if (args[0] == "say")
+
+if (args[0] != "read")
 {
-    var message = args[1];
-    var frequency = int.Parse(args[2]);
-    foreach (var i in Enumerable.Range(1, frequency))
-        Console.Write(message + " ");
-}*/
+    return;
+}
+try
+{
+    StreamReader input= new StreamReader(args[1]);
+    string line = input.ReadLine();
+
+    while (line != null)
+    {
+        line = input.ReadLine();
+        //time and date
+        int unixTime = int.Parse(line[Range.StartAt(line.LastIndexOf(",")+1)]);
+        DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(unixTime);
+        DateTimeOffset localtime =  date.LocalDateTime;
+        string prettyTime = localtime.ToString("MM/dd/yy HH:mm:ss").Replace("-", "/");
+        
+        //message
+        Range messageRange = new Range(line.IndexOf(",") + 2, line.LastIndexOf(",") - 1);
+        String message = line[messageRange];
+        //print
+        Console.WriteLine(line[Range.EndAt(4)] + " @ " + prettyTime + ": " + message);
+    }
+}
+catch (FileNotFoundException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+/* goal output:
+ropf @ 08/01/23 14:09:20: Hello, BDSA students!
+adho @ 08/02/23 14:19:38: Welcome to the course!
+adho @ 08/02/23 14:37:38: I hope you had a good summer.
+ropf @ 08/02/23 15:04:47: Cheeping cheeps on Chirp :) */
