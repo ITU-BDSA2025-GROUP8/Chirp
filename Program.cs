fe20ceb4bb9eb2d6513e8fea7a3bda5ed0a672cc
Frederik Petersen <frap@itu.dk>
2025-09-10 09:52:27 +0200

    Moved cheep record to own file
    Better for file management and maintinace

--- Program.cs
+++ Program.cs
@@ -37,29 +37,3 @@ else
     Console.WriteLine("Command not recognized");
 }
 
-// Cheep record consisting of author, message and timestamp
-public record Cheep
-{
-    public string Author { get; set; }
-    public string Message { get; set; }
-    public long Timestamp { get; set; }
-    
-    // ToString method. Format:
-    // ropf @ 08/01/23 14:09:20: Hello, BDSA students!
-    public override string ToString()
-    {
-        return Author + " @ " + PrettyTime() + ": " + Message;
-    }
-   
-    // Convert and format timestamp
-    string PrettyTime()
-    {
-        //time and date
-        long unixTime = Timestamp;
-        DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(unixTime);
-        DateTimeOffset localtime = date.LocalDateTime;
-        string prettyTime = localtime.ToString("MM/dd/yy HH:mm:ss").Replace("-", "/");
-        return prettyTime;
-    }
-    
-}
