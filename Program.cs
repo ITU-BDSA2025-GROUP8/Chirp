0f74f288a221d6eb7b1d62a467baaf03d3136624
Nikolej <nivl@itu.dk>
2025-09-10 11:56:57 +0200

    Started 2.d, Commandline library integration For easier commandline argument. Doesn't work atm. Will fix later
    Co-authored-by: Frederik Petersen <frap@itu.dk>
    Co-authored-by: Charlotte Planteig <cpla@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,36 +1,74 @@
 ﻿using Chirp.CLI.UserInterface;
 using SimpleDB;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.CommandLine.Invocation;
 
-var database = new CSVDatabase<Cheep>();
 
-if (args[0] == "read")
+namespace Chirp.CLI;
+class Program
 {
-    try
+    static void Main(string[] args)
     {
-        // Print all cheeps
-        UserInterface.printCheeps(database.Read());
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
+        // Initialize database
+        var database = new CSVDatabase<Cheep>();
+        
+        // Creating the root command - should automatically have a help and a version option and a suggets directive
+        RootCommand rootCommand = new("Sample app for System.CommandLine"); //todo: skriv en anden description
+       
+        //Create subcommands and adds them to the root command. 
+        var readCommand = new Command("read", "Reads current cheeps"); //todo: opdater potentielt beskrivelsen
+        rootCommand.Add(readCommand);
+        
+        
+        var cheepCommand = new Command("cheep", "create a new cheep");
+        var messageArg = new Argument<string>("message");
+        rootCommand.Add(cheepCommand);
+        cheepCommand.Add(messageArg);
+        
+        // Command handlers
+        cheepCommand.SetAction((ParseResult parseResult) =>
+        {
+            var message = parseResult.GetValue(messageArg);
+
+            Console.WriteLine($"writes: {message} ({DateTime.Now})");
+        });
+
+
+        
+        if (args[0] == "read")
+        {
+            try
+            {
+                // Print all cheeps
+                UserInterface.UserInterface.printCheeps(database.Read());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+        else if (args[0] == "cheep")
+        {
+            try
+            {
+                long time = DateTimeOffset.Now.ToUnixTimeSeconds();
+                Cheep cheep = new Cheep { Author = Environment.UserName, Message = args[1], Timestamp = time };
+                database.Store(cheep);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Command not recognized");
+        }
     }
     
-} else if (args[0] == "cheep")
-{
-    try
-    {
-        long time = DateTimeOffset.Now.ToUnixTimeSeconds();
-        Cheep cheep = new Cheep { Author = Environment.UserName, Message = args[1], Timestamp = time};
-        database.Store(cheep);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-    }
+    
+    
 }
-
-else
-{
-    Console.WriteLine("Command not recognized");
-}
-
