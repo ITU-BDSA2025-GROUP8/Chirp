94f6673be2b65e14e8846c352383c37d8532fd8d
Nikolej <nivl@itu.dk>
2025-09-10 10:21:47 +0200

    2.c completed Create user interface class To be expanded later
    Co-authored-by: Frederik Petersen <frap@itu.dk>
    Co-authored-by: Charlotte Planteig <cpla@itu.dk>

---
+++ UserInterface/UserInterface.cs
@@ -0,0 +1,13 @@
+﻿namespace Chirp.CLI.UserInterface;
+
+public static class UserInterface
+{
+    
+    public static void printCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach (var cheep in cheeps)
+        {
+            Console.WriteLine(cheep);
+        }
+    }
+}
