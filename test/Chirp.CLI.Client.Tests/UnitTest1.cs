bc738ddce4995670e27eae940b39447a842bd250
Nikolej <nivl@itu.dk>
2025-09-19 13:42:56 +0200

    Add small test of whole program. Purpose is to test the enviroment itself, but also test program works.
    Co-authored-by: Frederik Petersen <frap@itu.dk>

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -1,10 +1,28 @@
-﻿namespace Chirp.CLI.Client.Tests;
+﻿using Database;
+namespace Chirp.CLI.Client.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ReadFromDBTest()
     {
+        var database = new CSVDatabase<Cheep>();
 
+        Cheep test = new Cheep { Author = Environment.UserName, Message = "This is a test", Timestamp = 10000 };
+
+        database.Store(test);  
+        IEnumerable<Cheep> cheeps = database.Read();
+
+        Cheep c = null;
+        
+        foreach (var cheep in cheeps)
+        {
+            c = cheep;
+        }
+
+        Console.WriteLine(c);
+
+        Assert.Equal(c.Author, Environment.UserName);
+        Assert.Equal(c.Message, "This is a test");
     }
 }
