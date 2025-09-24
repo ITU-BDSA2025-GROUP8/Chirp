9563e2bb34718e5628380edd58269676122e35ca
Charlotte <cpla@itu.dk>
2025-09-24 11:15:53 +0200

    Rename test-class to CSVDatabase and test simple functionality
    Test that the database is initialized correctly and returns the correct instance

---
+++ test/Chirp.CSVDB.Tests/CSVDatabase.cs
@@ -0,0 +1,31 @@
+﻿using Database;
+
+namespace Chirp.CSVDB.Tests;
+
+public class CSVDatabase
+{
+    [Fact]
+    //tests that the database is initialized correctly
+    public void CorrectDatabaseInstanceTest()
+    {
+        var database = CSVDatabase<Cheep>.Instance; //get the instance of the database
+        Assert.NotNull(database); //tests that the database is not null
+        Assert.IsType<CSVDatabase<Cheep>>(database); //tests that the database is of type CSVDatabase<Cheep>
+    }
+    
+    [Fact]
+    //tests that the class returns the correct instance
+    public void CorrectInstanceTest()
+    {
+        var database1 = CSVDatabase<Cheep>.Instance;
+        var database2 = CSVDatabase<Cheep>.Instance;
+        Assert.Same(database1, database2); //tests that both instances are the same
+    }
+    
+    [Fact]
+    //tests that the class returns the correct instance
+    public void CorrectInstanceTest2()
+    {
+        var database1 = CSVDatabase<Cheep>.Instance;
+    }
+}
