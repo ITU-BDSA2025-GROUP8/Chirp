a0bde5a63d6ea3945b7489ca5cf9e39d7650356f
Frederik Petersen <frap@itu.dk>
2025-10-03 15:30:02 +0200

    Made test for methods in DBFacade.cs

--- Chirp.Razor.Tests/DBFacedeTest.cs
+++ Chirp.Razor.Tests/DBFacedeTest.cs
@@ -2,17 +2,76 @@
 
 public class DBFacedeTest
 {
+    
     private void Start()
     {
-        Environment.SetEnvironmentVariable("CHIRPDBPATH", "/tmp/chirp.db");
+        Environment.SetEnvironmentVariable("CHIRPDBPATH", "chirp.db");
     }
     
     
     [Fact]
-    public void DummyTest()
+    public void CanReadFromEnvironmentVariableTest()
     {
         Start();
         List<CheepViewModel> cheeps = DBFacade.Read();
         Assert.NotNull(cheeps);
     }
+
+    [Fact]
+    public void CanGetCorrectAuthorFromDB()
+    {
+        Start();
+        List<CheepViewModel> cheeps;
+        cheeps = DBFacade.Read();
+        Assert.Equal("Jacqualine Gilcoine",cheeps[0].Author);
+    }
+
+    [Fact]
+    public void CanGetCorrectMessageFromDB()
+    {
+        Start();
+        List<CheepViewModel> cheeps;
+        cheeps =  DBFacade.Read();
+        Assert.Equal("Starbuck now is what we hear the worst.", cheeps[0].Message);
+    }
+    
+    [Fact]
+    public void CanGetCorrectTimeFromDB()
+    {
+        Start();
+        List<CheepViewModel> cheeps;
+        cheeps = DBFacade.Read();
+        Assert.Equal("08-01-23 13:17:39",cheeps[0].Timestamp);
+    }
+
+    [Fact]
+    public void CanGetSpecificAuthorOnly()
+    {
+        Start();
+        List<CheepViewModel> cheeps;
+        cheeps = DBFacade.ReadAuthor("Jacqualine Gilcoine");
+        foreach (var cheep in cheeps)
+        {
+            Assert.Equal("Jacqualine Gilcoine", cheep.Author);
+        }
+    }
+    
+    [Theory]
+    [InlineData("Roger Histand")]
+    [InlineData("Luanna Muro")]
+    [InlineData("Wendell Ballan")]
+    [InlineData("Jacqualine Gilcoine")]
+    
+
+    public void OnlyGetSpecificAuthors(String author)
+    {
+        Start();
+        List<CheepViewModel> cheeps;
+        cheeps = DBFacade.ReadAuthor(author);
+        foreach (var cheep in cheeps)
+        {
+            Assert.Equal(author, cheep.Author);
+        }
+    }
+
 }
