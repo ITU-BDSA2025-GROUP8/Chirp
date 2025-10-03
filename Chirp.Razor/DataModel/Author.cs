6bc0192910aff8f95f072f28639eeece98524c23
Marie Johansen <majoh@itu.dk>
2025-10-03 11:33:57 +0200

    Make data model
    For implementing EF Core as per #42
    
    Co-authored-by: Sara <salb@itu.dk>
    Co-authored-by: Nikolej <nivl@itu.dk>

---
+++ Chirp.Razor/DataModel/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace Chirp.Razor.DataModel;
+
+public class Author
+{
+    public string  Name { get; set; }
+    public string  EmailAddress { get; set; }
+    public ICollection<Cheep> Cheeps { get; set; }
+}
---
+++ Chirp.Razor/DataModel/Cheep.cs
@@ -0,0 +1,8 @@
+﻿namespace Chirp.Razor.DataModel;
+
+public class Cheep
+{
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+    public Author Author { get; set; }
+}
