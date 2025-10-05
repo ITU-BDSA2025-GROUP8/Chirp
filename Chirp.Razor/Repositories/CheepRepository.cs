a52f3e09c5a13e81780ec78ed952f525f219589b
Marie Johansen <majoh@itu.dk>
2025-10-05 20:58:50 +0200

    (WIP) Initial implementation of UpdateCheep(alteredCheep) in CheepRepository.cs and refactor to using utility methods
    As per next step in #49 and to avoid code duplication
    Note: this code has not been run or tested

--- Chirp.Razor/Repositories/CheepRepository.cs
+++ Chirp.Razor/Repositories/CheepRepository.cs
@@ -18,13 +18,7 @@ public class CheepRepository : ICheepRepository
     // Create a new cheep todo: not run or tested
     public async Task CreateCheep(CheepDTO newCheep)
     {
-        // Suggestion from ChatGPT
-        // Gets one Author object from the database
-        var author = await (
-            from a in _context.Authors
-            where a.Name == newCheep.UserName 
-            select a
-            ).FirstOrDefaultAsync(); // Runs the query
+        var author = await findAuthor(newCheep.UserName);
         
         if (author == null)
         {
@@ -70,5 +64,61 @@ public class CheepRepository : ICheepRepository
             };
         var result = await query.ToListAsync();
         return result;
+    }
+
+    public async Task UpdateCheep(CheepDTO alteredCheep)
+    {
+        // Selects the original cheep from the database
+        var query = from cheep in _context.Cheeps
+            where cheep.CheepId == alteredCheep.Id
+            select cheep;
+        
+        var originalCheep = await query.FirstOrDefaultAsync();
+        
+        // Error handling
+        if (originalCheep == null)
+        {
+            throw new Exception("Unable to find the cheep");
+        }
+        
+        // Call to utility method that updates the properties of the original cheep
+        UpdateCheep(originalCheep, alteredCheep);
+        
+        // Saves changes
+        await _context.SaveChangesAsync();
+    }
+
+    // Utility methods todo: should they be in the repository?
+    
+    // Utility method to set the new properties of the Cheep
+    async void UpdateCheep(Cheep originalCheep, CheepDTO alteredCheep)
+    {
+        // Find the author object of the alteredCheep
+        var author = await findAuthor(alteredCheep.UserName);
+        
+        if (author == null)
+        {
+            throw new Exception($"Author with username '{alteredCheep.UserName}' not found while trying to update cheep.");
+        }
+        
+        // Sets the new properties
+        originalCheep.Author = author;
+        originalCheep.Text = alteredCheep.Text;
+        originalCheep.Date = alteredCheep.CreatedAt; //todo: is this a new time or the same as before?
+        originalCheep.CheepId = alteredCheep.Id;
+    }
+    // Utility methods to find the author object
+    //todo: right now the author object is found by the name - this should be changed to the ID as this is the key of an author object in the data model
+    async Task<Author> findAuthor(string AuthorName)
+    {
+        // Suggestion from ChatGPT
+        // Gets one Author object from the database
+        var author = await(
+            from a in _context.Authors
+            where a.Name == AuthorName
+            select a
+        ).FirstOrDefaultAsync(); // Runs the query and return the first author with that name or default value 
+
+        return author;
     }
 }
