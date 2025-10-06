ee82101b57491b436061629c101bb9f53604ab21
Marie Johansen <majoh@itu.dk>
2025-10-06 14:55:43 +0200

    (WIP) Initial implementation of CreateAuthor(Author) in AuthorRepository.cs #49

--- Chirp.Razor/Repositories/AuthorRepository.cs
+++ Chirp.Razor/Repositories/AuthorRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace Chirp.Razor.Repositories;
+﻿using Chirp.Razor.data;
+using Chirp.Razor.DataModel;
+using Chirp.Razor.Models;
 
-public class AuthorRepository
+namespace Chirp.Razor.Repositories;
+
+public class AuthorRepository : IAuthorRepository
 {
-    
+    private readonly ChirpDBContext _context;
+    public AuthorRepository(ChirpDBContext context)
+    {
+        _context = context;
+    }
+
+    // Create new author
+    public async Task CreateAuthor(AuthorDTO newUser)
+    {
+        // Creates new Author
+        Author author = new()
+        {
+            //todo: does not set AuthorId
+            Name = newUser.Name,
+            EmailAddress = newUser.Email,
+            Cheeps = new List<Cheep>() //todo: is this to be done in the repository
+        };
+
+        // Adds and saves the cheep in the database
+        var queryResult = await _context.Authors.AddAsync(author);
+        await _context.SaveChangesAsync();
+    }
 }
