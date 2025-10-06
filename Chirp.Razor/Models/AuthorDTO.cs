29b00ee95206ec5dd2a60efc5940e8965d4e324e
Marie Johansen <majoh@itu.dk>
2025-10-06 15:25:46 +0200

    Add Id to AuthorDTO.cs
    as to identitfy the specific author

--- Chirp.Razor/Models/AuthorDTO.cs
+++ Chirp.Razor/Models/AuthorDTO.cs
@@ -6,6 +6,7 @@ public class AuthorDTO
 {
     // public int Id { get; set; }
     // You could have one for ID but ID should probably be hidden from users
+    public int Id { get; internal set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public ICollection<Cheep> Cheeps { get; set; }
