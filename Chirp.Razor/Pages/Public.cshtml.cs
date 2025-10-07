c59a9f6be590dfe3680189e142ff161dc6e1cd4d
Charlotte <cpla@itu.dk>
2025-10-07 12:41:43 +0200

    Add comments to UserTimeline.cshtml.cs and Public.cshtml.cs
    This is done to make the code easier to read and/or understand for someone who did not implent it.
    
    Co-authored-by: Frederik <frap@itu.dk>

--- Chirp.Razor/Pages/Public.cshtml.cs
+++ Chirp.Razor/Pages/Public.cshtml.cs
@@ -3,16 +3,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
+//Pages for cheeps from all authors
 public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
 
+    //Inject the cheep service, sets a specific "model"
     public PublicModel(ICheepService service)
     {
         _service = service;
     }
 
+    //Get all cheeps by all authors
     public ActionResult OnGet()
     {
         Cheeps = _service.GetCheeps();
--- Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
+//Pages for cheeps from a specific author
 public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
 
+    //Inject the cheep service, sets a specific "model"
     public UserTimelineModel(ICheepService service)
     {
         _service = service;
