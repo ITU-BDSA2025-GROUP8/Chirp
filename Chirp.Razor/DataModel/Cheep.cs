45f554f4170aceabba16ff8bb3b6183c04b97344
Marie Johansen <majoh@itu.dk>
2025-10-10 10:50:17 +0200

    Make required length of Cheeps #56
    As migrations are not tracked by git, remember to add new migration and update database

--- Chirp.Razor/DataModel/Cheep.cs
+++ Chirp.Razor/DataModel/Cheep.cs
@@ -1,9 +1,14 @@
-﻿namespace Chirp.Razor.DataModel;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chirp.Razor.DataModel;
 
 public class Cheep
 {
+    //todo: add required fields
     public int CheepId { get; set; }
-    public string Text { get; set; }
+    [Required]
+    [StringLength(160)]
+    public required string Text { get; set; }
     public DateTime Date { get; set; }
     public Author Author { get; set; }
 }
