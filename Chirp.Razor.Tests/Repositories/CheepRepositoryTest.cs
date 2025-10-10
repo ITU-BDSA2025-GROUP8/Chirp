9fb66c8fb721b6994c22703d2409466580ba8e2b
Sara <salb@itu.dk>
2025-10-10 12:02:55 +0200

    make test for GetAllCheeps

--- Chirp.Razor.Tests/Repositories/CheepRepositoryTest.cs
+++ Chirp.Razor.Tests/Repositories/CheepRepositoryTest.cs
@@ -20,7 +20,8 @@ public class CheepRepositoryTest
         var options = new DbContextOptionsBuilder<ChirpDBContext>()
             .UseSqlite(connection)
             .Options;
-
+        
+        //Arrange
         using (var context = new ChirpDBContext(options))
         {
             context.Database.EnsureCreated(); 
@@ -44,7 +45,44 @@ public class CheepRepositoryTest
     [Fact]
     public void GetAllCheepsTest()
     {
-        
+        //Arrange
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+
+        var options = new DbContextOptionsBuilder<ChirpDBContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        using (var context = new ChirpDBContext(options))
+        {
+            context.Database.EnsureCreated();
+
+            //making authors to relate to the cheeps
+            var author1 = new Author { Name = "Test1", EmailAddress = "test@itu.dk" };
+            var author2 = new Author { Name = "Test2", EmailAddress = "test2@itu.dk" };
+
+            context.Authors.AddRange(author1, author2);
+            //making cheeps
+            context.Cheeps.AddRange(
+                new Cheep { Author = author1, Text = "hi", Date = new DateTime(2025, 10, 1) },
+                new Cheep { Author = author2, Text = "hello", Date = new DateTime(2025, 10, 2) }
+            );
+            context.SaveChanges();
+        }
+
+        using (var context = new ChirpDBContext(options))
+        {
+            var repository = new CheepRepository(context);
+
+            //Act
+            var allCheeps = repository.GetAllCheeps().Result;
+
+            //Assert
+            Assert.Equal(2, allCheeps.Count);
+            Assert.Contains(allCheeps, c => c.Text == "hi" && c.UserName == "Test1");
+            Assert.Contains(allCheeps, c => c.Text == "hello" && c.UserName == "Test2");
+            //Assert.All(allCheeps, c => Assert.True(c.Id > 0)); // Id should exist todo: check if this is needed?
+        }
     }
 
     [Fact]
