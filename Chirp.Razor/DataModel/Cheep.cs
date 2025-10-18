67cff7b9eb76af50b8ddf860b7ffd39ee9e4077b
Marie Johansen <majoh@itu.dk>
2025-10-18 20:01:30 +0200

    Add required properties to Cheep.cs #56

--- Chirp.Razor/DataModel/Cheep.cs
+++ Chirp.Razor/DataModel/Cheep.cs
@@ -4,11 +4,16 @@ namespace Chirp.Razor.DataModel;
 
 public class Cheep
 {
-    //todo: add required fields
+    [Key]
     public int CheepId { get; set; }
+    
     [Required]
     [StringLength(160)]
     public required string Text { get; set; }
-    public DateTime Date { get; set; }
-    public Author Author { get; set; }
+
+    [Required]
+    public required DateTime Date { get; set; }
+
+    [Required]
+    public required Author Author { get; set; }
 }
