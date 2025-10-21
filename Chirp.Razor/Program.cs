d41f0e9c9b772031a6c4c9565a9d08681534034a
Charlotte <cpla@itu.dk>
2025-10-21 16:35:19 +0200

    Seed the database per demand of issue #44

--- Chirp.Razor/Program.cs
+++ Chirp.Razor/Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddSingleton<ICheepService, CheepService>();
 
 var app = builder.Build();
 
+//Creates a scope to retrieve the database context, ensures the database exists, and seeds the database with example data
+//Code from https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-8.0&tabs=visual-studio-code#seed-the-database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ChirpDBContext>();
+    context.Database.EnsureCreated(); //todo: unsure if it should use EnsureCreated as the tutorial highlights but also says can't be used with migrations. If changed, change the comment above
+    DbInitializer.SeedDatabase(context);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
