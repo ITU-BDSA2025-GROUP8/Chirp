dfcad91697b46aea2859efd90551cfc2aa25adb6
Charlotte <cpla@itu.dk>
2025-10-21 15:22:00 +0200

    Add DbInitializer.cs and modify Cheep accordingly
    The DbInitializer was added to fit the issue. Then Cheep had to be modified, adding a paramether AuthorID.

--- Chirp.Razor/DataModel/Cheep.cs
+++ Chirp.Razor/DataModel/Cheep.cs
@@ -11,4 +11,5 @@ public class Cheep
     public required string Text { get; set; }
     public DateTime Date { get; set; }
     public Author Author { get; set; }
+    public int AuthorId { get; set; } //foreign key pointing to the author class
 }
