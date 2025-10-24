ca499b9355452bda68fe2be9b3aef91129b56839
Charlotte <cpla@itu.dk>
2025-10-24 12:07:03 +0200

    Set up the test class for db initializer
    This is done to make sure, that the DbInitializer works as intended.

---
+++ tests/Chirp.Infrastructure.Tests/Data/DbInitializerTest.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace Chirp.Infrastructure.Tests.Data;
+
+public class DbInitializerTest
+{
+    //test that the db initializer works - meaning that there is no database and it creates the correct one and the correct data is inserted
+    
+    //test that if there is a database, it is not overwritten - meaning that the data from the database is reachable and the data from the dbinitializer is not inserted
+}
