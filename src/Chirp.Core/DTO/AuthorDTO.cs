3cd06629a3dbd4fe53fa5ab87684fe0f676ccc59
Marie Johansen <majoh@itu.dk>
2025-10-30 16:14:59 +0100

    Make fields required or not null #60

--- src/Chirp.Core/DTO/AuthorDTO.cs
+++ src/Chirp.Core/DTO/AuthorDTO.cs
@@ -4,8 +4,8 @@ public class AuthorDTO
 {
     // public int Id { get; set; }
     // You could have one for ID but ID should probably be hidden from users
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public ICollection<CheepDTO> Cheeps { get; set; }
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public ICollection<CheepDTO> Cheeps { get; set; } = new List<CheepDTO>();
 }
