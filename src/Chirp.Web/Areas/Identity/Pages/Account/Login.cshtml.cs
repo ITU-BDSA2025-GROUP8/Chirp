0efdbdafa97314df7f99156f9f9a899e0812653c
Frederik Petersen <frap@itu.dk>
2025-10-31 11:02:54 +0100

    Fixed routing for login and logout
    Co-authored-by: Nikolej <nivl@itu.dk>

--- src/Chirp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,11 +21,13 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
     public class LoginModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<ApplicationUser> signInManager,UserManager<ApplicationUser> userManager, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
             _logger = logger;
         }
 
@@ -110,13 +112,24 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
+                
+                // Fetch user display name
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
+                
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    var displayname = user.DisplayName;
+                    return LocalRedirect("/" + displayname);
                 }
                 if (result.RequiresTwoFactor)
                 {
