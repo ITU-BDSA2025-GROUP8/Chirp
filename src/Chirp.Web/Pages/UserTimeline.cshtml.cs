8fdc01579fe7f2cdcccf041c5f104bd5ff3ed163
Charlotte <cpla@itu.dk>
2025-11-04 15:39:48 +0100

    Add write cheep functionality to UserTimeline

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Chirp.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Chirp.Web.Services;
 
@@ -9,19 +10,35 @@ public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
+    [BindProperty]
+    public string? CheepText { get; set; }
 
     //Inject the cheep service, sets a specific "model"
     public UserTimelineModel(ICheepService service)
     {
         _service = service;
     }
-    [BindProperty]
-    public string? Text { get; set; }
 
     //Gets all cheeps from a specific author
     public ActionResult OnGet(string author, [FromQuery] int page)
     {
         Cheeps = _service.GetCheepsFromAuthor(author, page);
         return Page();
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        //Create CheepDTO // todo: get the correct values, this is just dummy stuff
+        var cheepDTO = new CheepDTO()
+        {
+            CreatedAt = DateTime.Now,
+            Id = 1,
+            Text = CheepText,
+            UserName = "TestyTester" //Riders ide: HttpContext.User.Identity.Name 
+        };
+       
+        //Call the repository method for creating a cheep
+        await _service.CreateCheepFromDTO(cheepDTO);
+        return RedirectToPage();
     }
 }
