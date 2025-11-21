87f4aff9cf07c12cadcbe350397bf18828f94566
Marie Johansen <majoh@itu.dk>
2025-11-21 12:43:26 +0100

    Make test for XSS attacts with Playwright #87

---
+++ tests/Chirp.Web.Tests/PlaywrightUITests/PlaywrightSecurityTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace Chirp.Web.Tests.PlaywrightUITests;
+
+[TestFixture]
+public class PlaywrightSecurityTest: PageTest
+{
+    [Test] 
+    public async Task CheepboxCanSustainXSSAtacks() 
+    {
+        //todo: the test fails as it should be run when a user is logged in, so there is a cheep-box in the page
+        await Page.Locator("#CheepText").ClickAsync();
+        await Page.Locator("#CheepText").FillAsync("Hello, I am feeling good!<script>alert('If you see this in a popup, you are in trouble!');</script>"); //input text is directly from slides from lecture 10
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+        await Expect(Page.GetByText("TestyTester Hello, I am").First).ToBeVisibleAsync();
+        await Expect(Page.Locator("#messagelist")).ToContainTextAsync("TestyTester Hello, I am feeling good!<script>alert('If you see this in a popup, you are in trouble!');</script> — 21. november 2025");
+    }
+}
