7c0bef1708b2e97b4e9a2f3b17020d972ec71527
Nikolej <nivl@itu.dk>
2025-11-28 12:26:36 +0100

    Removed more comments

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -6,16 +6,6 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Chirp.Web.Pages;
 
-/*
- * Each user should have 2 timelines: A public and a private
- * The public contains all cheeps written by the user.
- * This is the timeline visible to other users.
- * 
- * The private contains all cheeps written by the user,
- * and all cheeps written by the authors that the user follows.
- * This timeline is only visible to the authenticated user.
- */
-
 //Pages for cheeps from a specific author
 public class UserTimelineModel : TimelineBaseModel
 {
