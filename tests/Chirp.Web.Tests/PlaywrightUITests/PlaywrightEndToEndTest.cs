1d9a99220b75600792afb8a1c60f8f917c2a8a63
Sara <salb@itu.dk>
2025-12-06 15:18:19 +0100

    Create PostCheep and correct details in register user

--- tests/Chirp.Web.Tests/PlaywrightUITests/PlaywrightEndToEndTest.cs
+++ tests/Chirp.Web.Tests/PlaywrightUITests/PlaywrightEndToEndTest.cs
@@ -18,11 +18,12 @@ public class PlaywrightEndToEndTest : PageTest
     { //todo: problem if testuser already exists
         await Page.GotoAsync(HomePage);
         await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
-        await Page.GetByPlaceholder("Name").FillAsync("Robert2");
-        await Page.GetByPlaceholder("Email").FillAsync("robert@test2.dk");
-        await Page.GetByPlaceholder("Password").FillAsync("Robert@test2.dk1");
+        await Page.GetByPlaceholder("Username").FillAsync("Robert2");
+        await Page.GetByPlaceholder("name@example.com").FillAsync("robert@test2.dk");
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Robert@test.dk2");
+        await Page.GetByLabel("Confirm Password").FillAsync("Robert@test.dk2");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
-        
+
     }
 //log in with testuser
     private async Task Login()
@@ -30,13 +31,25 @@ public class PlaywrightEndToEndTest : PageTest
         await Page.GotoAsync(HomePage);
         await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
         await Page.GetByPlaceholder("name@example.com").FillAsync("robert@test2.dk");
-        await Page.GetByPlaceholder("password").FillAsync("Robert@test2.dk");
+        await Page.GetByPlaceholder("password").FillAsync("Robert@test.dk2");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
 
     }
-    [Test]
-    public async Task cheepInDatabase()
-    {
 
+    private async Task PostCheep(string cheep)
+    {
+        await Page.GotoAsync(HomePage);
+        Console.WriteLine(await Page.ContentAsync());
+        await Page.Locator("#CheepText").FillAsync(cheep);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+    }
+    [Test]
+    public async Task RegisterNewUser_AllowsCheeping()
+    {
+        await Register();
+        await Login();
+        await PostCheep("Hi I'm a newly registered user");
+        
+        Assert.That(await Page.ContentAsync(), Does.Contain("Hi I'm a newly registered user"));
     }
 }
