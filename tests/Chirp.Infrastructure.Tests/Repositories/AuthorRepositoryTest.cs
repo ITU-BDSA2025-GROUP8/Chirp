91983cfac21633825d0213463f197f3c8d632869
Frederik Petersen <frap@itu.dk>
2025-12-07 14:58:59 +0100

    new fix to stop workflow build fail

--- tests/Chirp.Infrastructure.Tests/Repositories/AuthorRepositoryTest.cs
+++ tests/Chirp.Infrastructure.Tests/Repositories/AuthorRepositoryTest.cs
@@ -136,9 +136,9 @@ public class AuthorRepositoryTest : IDisposable
         context.Authors.AddRange(a1, a2);
         
         context.Cheeps.AddRange(
-            new Cheep { Author = a1, Text = "hello", Date = new DateTime(2025, 10, 10),LikedBy = [] },
-            new Cheep { Author = a1, Text = "world", Date = new DateTime(2025, 10, 11),LikedBy = [] },
-            new Cheep { Author = a2, Text = "cheep", Date = new DateTime(2025, 10, 12),LikedBy = [] }
+            new Cheep { Author = a1, Text = "hello", Date = new DateTime(2025, 10, 10),LikedBy = new List<string>() },
+            new Cheep { Author = a1, Text = "world", Date = new DateTime(2025, 10, 11),LikedBy = new List<string>() },
+            new Cheep { Author = a2, Text = "cheep", Date = new DateTime(2025, 10, 12),LikedBy = new List<string>() }
         );
         context.SaveChanges();
         
@@ -166,9 +166,9 @@ public class AuthorRepositoryTest : IDisposable
         context.Authors.AddRange(a1, a2);
         
         context.Cheeps.AddRange(
-            new Cheep { Author = a1, Text = "hello", Date = new DateTime(2025, 10, 10),LikedBy = [] },
-            new Cheep { Author = a1, Text = "world", Date = new DateTime(2025, 10, 11),LikedBy = [] },
-            new Cheep { Author = a2, Text = "cheep", Date = new DateTime(2025, 10, 12),LikedBy = [] }
+            new Cheep { Author = a1, Text = "hello", Date = new DateTime(2025, 10, 10),LikedBy = new List<string>() },
+            new Cheep { Author = a1, Text = "world", Date = new DateTime(2025, 10, 11),LikedBy = new List<string>() },
+            new Cheep { Author = a2, Text = "cheep", Date = new DateTime(2025, 10, 12),LikedBy = new List<string>() }
         );
         context.SaveChanges();
         var repository = new AuthorRepository(context);
