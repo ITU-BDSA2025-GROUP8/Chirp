9fcfb9a948d55e5e161fdbd12ba5e431f67f4e82
Marie Johansen <majoh@itu.dk>
2025-12-21 17:25:08 +0100

    Fix corrupted migration #114

--- src/Chirp.Infrastructure/Migrations/20251213150650_RemoveIsDeletedPropertyFromEntities.cs
+++ src/Chirp.Infrastructure/Migrations/20251213150650_RemoveIsDeletedPropertyFromEntities.cs
@@ -10,20 +10,11 @@ namespace Chirp.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "LikedBy",
-                table: "Cheeps",
-                type: "TEXT",
-                nullable: false,
-                defaultValue: "[]");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "LikedBy",
-                table: "Cheeps");
         }
     }
 }
