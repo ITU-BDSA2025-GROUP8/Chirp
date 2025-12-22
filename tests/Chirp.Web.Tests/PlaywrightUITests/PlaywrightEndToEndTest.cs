84ba3af8e2a8d268a6e55e5a85230970e4389ed5
Sara <salb@itu.dk>
2025-12-22 23:07:25 +0100

    login parameters made uniform and fixed unused imports

--- tests/Chirp.Web.Tests/PlaywrightUITests/PlaywrightEndToEndTest.cs
+++ tests/Chirp.Web.Tests/PlaywrightUITests/PlaywrightEndToEndTest.cs
@@ -1,6 +1,4 @@
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Playwright;
-using Chirp.Web.Tests.PlaywrightUITests;
 
 namespace Chirp.Web.Tests.PlaywrightUITests;
 
@@ -18,10 +16,10 @@ public class PlaywrightEndToEndTest : PageTest
     //Helpers
     //Registering with test user
 
-    private async Task Register()
+    private async Task <(string Email, string Password)> Register()
     {
         //Generate unique suffix
-        var unique = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        var unique = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
         
         var uniqueUsername = $"Test{unique}";
         var uniqueEmail = $"{uniqueUsername}@test.dk";
@@ -30,20 +28,21 @@ public class PlaywrightEndToEndTest : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
         await Page.GetByPlaceholder("Username").FillAsync(uniqueUsername);
         await Page.GetByPlaceholder("name@example.com").FillAsync(uniqueEmail);
-        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Robert@test.dk2");
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync(TestPassword);
         await Page.GetByLabel("Confirm Password").FillAsync(TestPassword);
         await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
 
+        return (uniqueEmail, TestPassword);
     }
 
 //log in with test user
-    private async Task Login()
+    private async Task Login(string email, string password)
     {
         
         await Page.GotoAsync(HomePage);
         await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").FillAsync(TestEmail);
-        await Page.GetByPlaceholder("password").FillAsync(TestPassword);
+        await Page.GetByPlaceholder("name@example.com").FillAsync(email);
+        await Page.GetByPlaceholder("password").FillAsync(password);
         await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
 
     }
@@ -59,8 +58,8 @@ public class PlaywrightEndToEndTest : PageTest
     [Test]
      public async Task RegisterNewUser_AllowsCheeping()
      {
-         await Register();
-         await Login();
+         var (email, password) = await Register();
+         await Login(email, password);
          await PostCheep("Hi I'm a newly registered user");
          
          Assert.That(await Page.ContentAsync(), Does.Contain("Hi I'm a newly registered user"));
@@ -71,16 +70,17 @@ public class PlaywrightEndToEndTest : PageTest
     [Test]
     public async Task Login_AllowsCheeping()
     {
-        await Login();
+        await Login(TestEmail, TestPassword);
         var cheepMessage = "Cheep from known user";
         await PostCheep(cheepMessage);
 
         var content = await Page.ContentAsync();
         Assert.That(content.Contains(cheepMessage));
     }
+    [Test]
      public async Task Login_CanLikeCheep() 
      {
-         await Login();
+         await Login(TestEmail, TestPassword);
          await Page.GotoAsync(HomePage);
          
          //Click first like button
