fed91deceba348f4b5e5ad4290e0a2581b6c8865
Sara <salb@itu.dk>
2025-12-22 23:01:45 +0100

    fix of registration of unique user

--- tests/Chirp.Web.Tests/PlaywrightUITests/PlaywrightEndToEndTest.cs
+++ tests/Chirp.Web.Tests/PlaywrightUITests/PlaywrightEndToEndTest.cs
@@ -12,21 +12,26 @@ using NUnit.Framework;
 public class PlaywrightEndToEndTest : PageTest
 {
     private const string HomePage = "https://bdsa2024group8chirprazor2025.azurewebsites.net/";
-    private const string TestEmail = "test@test.dk";
-    private const string TestPassword = "Test@test.dk1";
+    private const string TestEmail = "robert@test.dk";
+    private const string TestPassword = "Robert@test.dk1";
 
     //Helpers
     //Registering with test user
 
     private async Task Register()
     {
-        //todo: problem if test user already exists
+        //Generate unique suffix
+        var unique = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        
+        var uniqueUsername = $"Test{unique}";
+        var uniqueEmail = $"{uniqueUsername}@test.dk";
+        
         await Page.GotoAsync(HomePage);
         await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
-        await Page.GetByPlaceholder("Username").FillAsync("Robert2");
-        await Page.GetByPlaceholder("name@example.com").FillAsync("robert@test2.dk");
+        await Page.GetByPlaceholder("Username").FillAsync(uniqueUsername);
+        await Page.GetByPlaceholder("name@example.com").FillAsync(uniqueEmail);
         await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Robert@test.dk2");
-        await Page.GetByLabel("Confirm Password").FillAsync("Robert@test.dk2");
+        await Page.GetByLabel("Confirm Password").FillAsync(TestPassword);
         await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
 
     }
@@ -50,16 +55,16 @@ public class PlaywrightEndToEndTest : PageTest
         await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
     }
     
-    //todo: fix registration
-    //[Test]
-    // public async Task RegisterNewUser_AllowsCheeping()
-    // {
-    //     await Register();
-    //     await Login();
-    //     await PostCheep("Hi I'm a newly registered user");
-    //     
-    //     Assert.That(await Page.ContentAsync(), Does.Contain("Hi I'm a newly registered user"));
-    // }
+    
+    [Test]
+     public async Task RegisterNewUser_AllowsCheeping()
+     {
+         await Register();
+         await Login();
+         await PostCheep("Hi I'm a newly registered user");
+         
+         Assert.That(await Page.ContentAsync(), Does.Contain("Hi I'm a newly registered user"));
+     }
 
     //follow link for specific author
 
@@ -73,16 +78,16 @@ public class PlaywrightEndToEndTest : PageTest
         var content = await Page.ContentAsync();
         Assert.That(content.Contains(cheepMessage));
     }
-    // public async Task Login_CanLikeCheep() //todo can't be done until like is on deployed
-    // {
-    //     await Login();
-    //     await Page.GotoAsync(HomePage);
-    //     
-    //     //Click first like button
-    //     var firstLikeButton = Page.GetByRole(AriaRole.Link, new() { Name = "Like" });
-    //     await firstLikeButton.ClickAsync();
-    //     //Assert that page contains liked
-    //     var content = await Page.ContentAsync();
-    //     Assert.That(content.Contains("Liked"));
-    // }
+     public async Task Login_CanLikeCheep() 
+     {
+         await Login();
+         await Page.GotoAsync(HomePage);
+         
+         //Click first like button
+         var firstLikeButton = Page.GetByRole(AriaRole.Link, new() { Name = "Like" });
+         await firstLikeButton.ClickAsync();
+         //Assert that page contains liked
+         var content = await Page.ContentAsync();
+         Assert.That(content.Contains("Liked"));
+     }
 }
