ea74efc23d24792e085fbe7844988a3c6c868ab9
Marie Johansen <majoh@itu.dk>
2025-12-23 09:52:26 +0100

    Code cleanup #113
    Co-authored-by: Charlotte Planteig <cpla@itu.dk>

--- src/Chirp.Core/Interfaces/IAuthorRepository.cs
+++ src/Chirp.Core/Interfaces/IAuthorRepository.cs
@@ -4,20 +4,12 @@ namespace Chirp.Core.Interfaces;
 
 public interface IAuthorRepository
 {
-    // Create a new author
     Task CreateAuthor(AuthorDTO newUser);
-    // Get all authors
     Task<List<AuthorDTO>> GetAllAuthors();
-    // Update author
     Task UpdateAuthor(AuthorDTO updatedAuthor);
-    // Find author by name
     Task<AuthorDTO?> FindByName(string name);
-    // Find author by email
     Task<AuthorDTO?> FindByEmail(string email);
-    // Follow user
     Task FollowUser(AuthorDTO self,string followAuthorUsername);
-    // Unfollow user
     Task UnFollowUser(AuthorDTO self,string followAuthorUsername);
-    // Delete author
     Task DeleteAuthor(AuthorDTO author);
 }
