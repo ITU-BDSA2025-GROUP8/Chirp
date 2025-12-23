07c7c8fd191e3b944353a33239252b438c19f69f
Marie Johansen <majoh@itu.dk>
2025-12-23 09:53:05 +0100

    Code cleanup #113
    Co-authored-by: Charlotte Planteig <cpla@itu.dk>

--- src/Chirp.Core/Interfaces/ICheepRepository.cs
+++ src/Chirp.Core/Interfaces/ICheepRepository.cs
@@ -4,18 +4,11 @@ namespace Chirp.Core.Interfaces;
 
 public interface ICheepRepository
 {
-    // Create a new cheep
     Task CreateCheep(CheepDTO newCheep);
-    // Read all cheeps
     Task<List<CheepDTO>> GetAllCheeps(int? page = null);
-    // Read cheeps by a specific author
     Task<List<CheepDTO>> ReadCheepsBy(string authorName, int? page = null);
-    //Read cheeps by a specific author on one page
     Task<List<CheepDTO>> ReadCheepsByOnOnePage(string authorName);
-    // Read cheeps by yourself and given authors
     Task<List<CheepDTO>> ReadCheepsBySelfAndOthers(IList<string> authorNames, int? page = null);
-    // Update an existing cheep
     Task UpdateCheep(CheepDTO alteredCheep);
-    // Get a specific cheep
     Task<CheepDTO> GetCheep(int id);
 }
